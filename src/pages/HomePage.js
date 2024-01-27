7c2eae1624301b64be265145e55b447a38c1f8d0
Brandon <brandontang0325@gmail.com>
2024-01-27 23:25:56 +0000

    improve HomePage styling
    add styles for scrolling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import globalStyles from "../globalStyles";
 import WaveItem from "../components/WaveItem";
 import { gray500 } from "../constants";
@@ -23,28 +23,37 @@ const waves = [
 ];
 
 export default function HomePage() {
-const {setTitle} = usePage()
+  const { setTitle } = usePage();
   return (
     <View style={styles.homePage}>
-      <View style={styles.waveListContainer}>
-        {waves.map((wave, i) => (
-          <>
-            {i !== 0 && <View style={styles.separator} />}
-            <WaveItem
-              title={wave.title}
-              timeRange={wave.timeRange}
-              color={wave.color}
-            />
-          </>
-        ))}
+      <ScrollView
+        style={styles.waveListScrollContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.waveListContainer}>
+          {waves.map((wave, i) => (
+            <>
+              {i !== 0 && <View style={styles.separator} />}
+              <WaveItem
+                title={wave.title}
+                timeRange={wave.timeRange}
+                color={wave.color}
+              />
+            </>
+          ))}
+        </View>
+      </ScrollView>
+      <View style={styles.buttonContainer}>
+        <Pressable
+          style={[globalStyles.button, globalStyles.shadow]}
+          onPress={() => setTitle("edit")}
+        >
+          <Text style={globalStyles.text}>
+            <Text style={globalStyles.textPurple}>new </Text>
+            <Text style={globalStyles.textBlue}>Wave</Text>
+          </Text>
+        </Pressable>
       </View>
-
-      <Pressable style={[globalStyles.button, globalStyles.shadow]} onPress={()=>setTitle("edit")}>
-        <Text style={globalStyles.text}>
-          <Text style={globalStyles.textPurple}>new </Text>
-          <Text style={globalStyles.textBlue}>Wave</Text>
-        </Text>
-      </Pressable>
     </View>
   );
 }
@@ -53,16 +62,23 @@ const styles = StyleSheet.create({
   homePage: {
     alignItems: "center",
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
   },
-  waveListContainer: {
+  waveListScrollContainer: {
     alignSelf: "stretch",
     flex: 1,
+  },
+  waveListContainer: {
+    marginTop: 50,
   },
   separator: {
     height: 16,
     borderColor: gray500,
     borderLeftWidth: 1,
     marginLeft: 17,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 16,
   },
 });
