0a8b7ac00c9a19c7c3c75a7f86e1d095cbc245c0
Brandon <brandontang0325@gmail.com>
2024-02-07 21:45:50 +0000

    Fix request url

--- src/utils/post.js
+++ src/utils/post.js
@@ -1,6 +1,6 @@
 export async function postWaves(waves, ip) {
   console.log(JSON.stringify(waves));
-  const response = await fetch(ip, {
+  const response = await fetch(`http://${ip}/`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -8,4 +8,17 @@ export async function postWaves(waves, ip) {
     body: JSON.stringify(waves),
   });
   return response.json();
+}
+
+export async function testResponse(ip) {
+  const response = await fetch(`http://${ip}/`, {
+    method: "GET",
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch IP`);
+  }
+
+  const data = await response.text();
+  return data;
 }
